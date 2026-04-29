4dab0b4190085061bac4783637f58ee2c5204aad
t-cool <t-cool@mac.lan>
2026-04-29 20:38:55 +0900

    update

--- src/components/Hint/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { type ReactNode } from "react";
-import Details from "@theme/Details";
-
-/**
- * Hint component
- */
-export default function Hint({ children }: { children: ReactNode }) {
-  return <Details summary={<summary>ヒント</summary>}>{children}</Details>;
-}
